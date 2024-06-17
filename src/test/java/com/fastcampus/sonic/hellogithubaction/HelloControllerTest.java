007a1c8bcfc22244cdee64ea01e4b57dab5e18eb
RATMJM <104188653+RATMJM@users.noreply.github.com>
2024-06-17 21:32:23 +0900

    Update hellController

--- src/test/java/com/fastcampus/sonic/hellogithubaction/HelloControllerTest.java
+++ src/test/java/com/fastcampus/sonic/hellogithubaction/HelloControllerTest.java
@@ -30,7 +30,7 @@ class HelloControllerTest {
     @Test
     void helloTest() throws Exception {
         // given
-        String testMessage = "Hello, Github Actions";
+        String testMessage = "Hello, Github Actions by jmm";
 
         // when-then
         mockMvc.perform(get("/hello"))
